9696f06b3bff6bd56f388335fa7e945d2e75bafb
David <deverczsk@gmail.com>
2025-04-02 00:58:33 +0200

    next progress

--- app/js/slider.js
+++ app/js/slider.js
@@ -1,1 +1,15 @@
-function mainSlider() {}
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
